d72858c1013564d153ed6aad4121b8da19a7ea64
George Lemmon <george.lemmon@gmail.com>
2022-03-24 16:46:41 -0600

    Added an event listener to poster, added key to state, add handler to app

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies
-    }
+      movies: movieData.movies,
+      singleMovieView: null,
+    };
   }
+
+  toggleFocusView = (id) => this.setState({ singleMovieView: id });
 
   render() {
     return (
       <div>
         <Header />
         <main>
-          <Movies movies={this.state.movies}/>
+          <Movies movies={this.state.movies} toggleFocusView={this.toggleFocusView} />
         </main>
       </div>
     );
