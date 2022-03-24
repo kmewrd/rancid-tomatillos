6936a6e49d9d60b95ebec1f670a613488d1fae42
Kim Ward <kmewrd@outlook.com>
2022-03-24 15:50:13 -0500

    Add background color, poster images, and grid layout

--- src/Movies.js
+++ src/Movies.js
@@ -6,7 +6,7 @@ const Movies = ({movies}) => {
   const allMovies = movies.map(movie => <Poster key={movie.id} id={movie.id} title={movie.title} avgRating={movie.average_rating} releaseDate={movie.release_date} posterImg={movie.poster_path} backdropImg={movie.backdrop_path}/>)
 
   return (
-    <div>
+    <div className="movie-container">
       {allMovies}
     </div>
   )
--- src/Poster.js
+++ src/Poster.js
@@ -3,7 +3,8 @@ import './Poster.css';
 
 const Poster = ({id, title, avgRating, releaseDate, posterImg, backdropImg}) => {
   return (
-    <div id={id}>
+    <div id={id} className="movie-poster">
+      <img src={posterImg} className="poster-image"/>
       <h2>{title}</h2>
       <h3>{releaseDate}</h3>
     </div>
