44586bb5bb4081eca4267583cf84becbb40eac83
Kim Ward <kmewrd@outlook.com>
2022-03-25 17:17:43 -0500

    Write fetching function for single movie endpoint

--- src/APIcalls.js
+++ src/APIcalls.js
@@ -1,10 +1,10 @@
 const fetchAPI = {
   fetchMovies() {
-    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then((response) =>
-    response.json(),
-    );
+    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then((response) => response.json());
   },
-
+  fetchSingleMovie(id) {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`).then((response) => response.json());
+  }
 }
 
 export default fetchAPI;
