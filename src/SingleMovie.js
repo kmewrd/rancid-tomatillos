bc962c419ea81de93fb17c64b2475d37f7cc2d0c
Kim Ward <kmewrd@outlook.com>
2022-03-25 18:07:24 -0500

    Refactor conditional render with variables

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -18,14 +18,24 @@ class SingleMovie extends Component {
   componentDidMount = () => this.getSingleMovie(this.props.id);
 
   render() {
-    return (
-      <div>
-        {!this.state.movie ? <p>Sorry, please try again later.</p> : <div><h1>{this.state.movie.title}</h1>
-          <img src={this.state.movie.backdrop_path} />
+    let movie = this.state.movie;
+    let movieDetails;
+    
+    if (movie) {
+      movieDetails =
+        <div>
+          <h1>{this.state.movie.title}</h1>
+          <img src={this.state.movie.backdrop_path} alt='' />
           <h2>Rating: {this.state.movie.average_rating.toFixed(1)}</h2>
           <h3>{this.state.movie.release_date}</h3>
           <p>{this.state.movie.overview}</p>
-          <button onClick={this.props.focusViewOff}>Return to main</button></div>}
+          <button onClick={this.props.focusViewOff}>Return to main</button>
+        </div>
+    }
+
+    return (
+      <div>
+        {!this.state.movie ? <p>Sorry, please try again later.</p> : movieDetails}
       </div>
     );
   }
