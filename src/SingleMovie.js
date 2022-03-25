b13c3a6ad229e06378f0bb08b5cb2f0fb6b7e0b5
Kim Ward <kmewrd@outlook.com>
2022-03-25 13:05:22 -0500

    Change margin/layout for modal text and add button rules

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -4,15 +4,20 @@ import './SingleMovie.css';
 const SingleMovie = ( {focusViewOff} ) => {
   return (
     <div className='modal-window'>
+      <button onClick={focusViewOff} className='back-button'>Return to main</button>
       <img src="https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg" />
-      <h1>Fake Movie Title</h1>
-      <h2>Average Rating: 6</h2>
-      <h3>2019</h3>
+      <div className='title-and-rating'>
+        <h1>Fake Movie Title</h1>
+        <h2><span className='green-text'>Rating: 6</span></h2>
+      </div>
+      <div className='year-and-genres'>
+        <h3>2019</h3>
+        <h3>Genres: Romance</h3>
+      </div>
       <p>
         Some overview that is full of buzzwords to attempt to entice you to
         watch this movie! Explosions! Drama! True love! Robots! A cute dog!
       </p>
-      <button onClick={focusViewOff} >Return to main</button>
     </div>
   );
   
