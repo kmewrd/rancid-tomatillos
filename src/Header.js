a07f2cb2f33c94d584f6dd03bc327201568fc4b9
George Lemmon <george.lemmon@gmail.com>
2022-03-29 15:19:48 -0600

    Add return to main funcitonality to header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
-      <h1 className='logotype'>Rancid <span className='green-text'>Tomatillos</span></h1>
+      <Link to="/" ><h1 className='logotype'>Rancid <span className='green-text'>Tomatillos</span></h1></Link>
     </header>
   );
 }
