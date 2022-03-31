c3edaa4e0f340e5a55c6828c3a56c278e27b044f
Kim Ward <kmewrd@outlook.com>
2022-03-31 15:34:01 -0500

    Stub all network requests for homepage tests

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -2,18 +2,24 @@ import allMovies from './data.js'
 
 describe('Home Page', () => {
   it('Should have a heading containing the title of the website (Rancid Tomatillos).', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', allMovies);
+
     cy.visit('http://localhost:3000/')
       .get('header')
       .contains('Rancid Tomatillos');
   });
 
   it('Should contain a grid of movies posters.', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', allMovies);
+    
     cy.visit('http://localhost:3000/')
       .get('.movie-container div:first')
       .should('have.class', 'movie-poster');
   });
 
   it('Movie posters should have an image, title, and release year.', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', allMovies);
+    
     cy.visit('http://localhost:3000/')
       .get('div[id="694919"]')
       .should('contain', 'Money Plane')
