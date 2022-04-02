1270dd00d08fcdf683d433ce389c6da5ce1c4bbb
George Lemmon <george.lemmon@gmail.com>
2022-04-02 11:28:54 -0600

    Add template for sort form

---
+++ src/SortMenu.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import './SortMenu.css';
+
+class SortMenu extends Component {
+  constructor () {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <form>
+
+      </form>
+    )
+  }
+}
+
+export default SortMenu
+
+
+
