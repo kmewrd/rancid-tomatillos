cec2001c673cf168747c5e198d9b4b584e9a3182
George Lemmon <george.lemmon@gmail.com>
2022-04-02 11:54:29 -0600

    Add state chenge based on menus

--- src/SortMenu.js
+++ src/SortMenu.js
@@ -4,26 +4,35 @@ import './SortMenu.css';
 class SortMenu extends Component {
   constructor () {
     super()
-    this.state = {}
+    this.state = {
+      sort: "a-to-z",
+      filter: ""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
   }
 
   render() {
     return (
       <form>
         <label for="sort">Sort:</label>
-        <select name="sort">
+        <select name="sort" onChange={(e) => this.handleChange(e)}>
           <option value="a-to-z">Alphabetically (A-Z)</option>
           <option value="z-to-a">Alphabetically (Z-A)</option>
           <option value="ascending-rating">By Rating Ascending</option>
           <option value="descending-rating">By Rating Descending</option>
         </select>
         <label for="filter">Filter:</label>
-        <select name="filter" >
-          <option value="" disabled selected>Please select an option</option>
-          <option value="above-6" >Ratings above 6</option>
-          <option value="above-7" >Ratings above 7</option>
-          <option value="above-8" >Ratings above 8</option>
-          <option value="above-9" >Ratings above 9</option>
+        <select name="filter" onChange={(e) => this.handleChange(e) }>
+          <option value="" selected>
+            Show all ratings
+          </option>
+          <option value="above-6">Ratings above 6</option>
+          <option value="above-7">Ratings above 7</option>
+          <option value="above-8">Ratings above 8</option>
+          <option value="above-9">Ratings above 9</option>
         </select>
       </form>
     );
