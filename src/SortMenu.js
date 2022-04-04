588af1eb75b10ba424e906c007e72f63ca4f58fa
George Lemmon <george.lemmon@gmail.com>
2022-04-04 14:00:17 -0600

    Add styling and spacing drop down menus

--- src/SortMenu.js
+++ src/SortMenu.js
@@ -27,24 +27,36 @@ class SortMenu extends Component {
 
   render() {
     return (
-      <form>
-        <label htmlFor="sort">Sort:</label>
-        <select name="sort" onChange={(e) => this.handleChange(e)} value={this.state.sort}>
-          <option value="a-to-z">Alphabetically (A-Z)</option>
-          <option value="z-to-a">Alphabetically (Z-A)</option>
-          <option value="ascending-rating">By Rating Ascending</option>
-          <option value="descending-rating">By Rating Descending</option>
-        </select>
-        <label htmlFor="filter">Filter:</label>
-        <select name="filter" onChange={(e) => this.handleChange(e)} value={this.state.filter}>
-          <option value='none' >
-            Show all ratings
-          </option>
-          <option value="above-5">Ratings above 5</option>
-          <option value="above-6">Ratings above 6</option>
-          <option value="above-7">Ratings above 7</option>
-        </select>
-        <button className='clear-button' onClick={e => this.resetMenu(e)}>Reset</button>
+      <form className="filter-menu">
+        <div className="sort-drop">
+          <label htmlFor="sort">Sort:</label>
+          <select
+            name="sort"
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.sort}
+          >
+            <option value="a-to-z">Alphabetically (A-Z)</option>
+            <option value="z-to-a">Alphabetically (Z-A)</option>
+            <option value="ascending-rating">By Rating Ascending</option>
+            <option value="descending-rating">By Rating Descending</option>
+          </select>
+        </div>
+        <div className="filter-drop">
+          <label htmlFor="filter">Filter:</label>
+          <select
+            name="filter"
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.filter}
+          >
+            <option value="none">Show all ratings</option>
+            <option value="above-5">Ratings above 5</option>
+            <option value="above-6">Ratings above 6</option>
+            <option value="above-7">Ratings above 7</option>
+          </select>
+        </div>
+        <button className="clear-button" onClick={(e) => this.resetMenu(e)}>
+          Reset
+        </button>
       </form>
     );
   }
