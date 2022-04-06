96cb7fb7b30479670f2c9aaa3fb60e2e9d3784e1
Kim Ward <kmewrd@outlook.com>
2022-04-06 16:44:07 -0500

    Change logotype from Link to NavLink

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Header.css';
 import SortMenu from './SortMenu';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = ({ updateRenderedMovies }) => {
   return (
     <header>
-      <Link to="/" style={{ textDecoration: 'none', color: '#fff' }}>
+      <NavLink to="/" style={{ textDecoration: 'none', color: '#fff' }}>
         <h1 className='logotype'>
           Rancid <span className='green-text'>Tomatillos</span>
         </h1>
-      </Link>
+      </NavLink>
       <SortMenu updateRenderedMovies={updateRenderedMovies} />
     </header>
   );
